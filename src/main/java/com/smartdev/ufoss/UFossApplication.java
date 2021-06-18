f0571be2736575a2a1aa7c12c8c0c4dbb4cb950a
tamaumi <huynhngochai20101998@gmail.com>
2021-06-18 13:59:53 +0700

    first commit

---
+++ src/main/java/com/smartdev/ufoss/UFossApplication.java
@@ -0,0 +1,13 @@
+package com.smartdev.ufoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UFossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UFossApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartdev/ufoss/UFossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdev.ufoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UFossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
