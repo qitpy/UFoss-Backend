efbd4c4b55fd985377bcbcadaf9fcb6c7dfaa54a
Thiet <thiettruong1999@gmail.com>
2021-07-11 13:35:12 +0700

    done mycourse-api

--- src/main/java/com/smartdev/ufoss/service/impI/MyCourseServiceImpl.java
+++ src/main/java/com/smartdev/ufoss/service/impI/MyCourseServiceImpl.java
@@ -1,46 +1,30 @@
 package com.smartdev.ufoss.service.impI;
-import com.smartdev.ufoss.converter.PaymentConverter;
-import com.smartdev.ufoss.dto.PaymentGetDTO;
 import com.smartdev.ufoss.entity.CourseEntity;
-import com.smartdev.ufoss.entity.PaymentEntity;
 import com.smartdev.ufoss.entity.UserEntity;
-import com.smartdev.ufoss.repository.CoursesRepository;
-import com.smartdev.ufoss.repository.PaymentRepository;
+import com.smartdev.ufoss.exception.HandleException;
 import com.smartdev.ufoss.repository.UserRepository;
 import com.smartdev.ufoss.service.MyCourseSevice;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 
 @AllArgsConstructor
 @Service
 
 public class MyCourseServiceImpl implements MyCourseSevice {
-    private PaymentRepository paymentRepository;
     private UserRepository userRepository;
-    private CoursesRepository coursesRepository;
+
     @Override
     public List<CourseEntity> getMyCourseByUserId(UUID userId) {
-        System.out.println("no o day ");
+
         List<CourseEntity> courseEntities = new ArrayList<>();
-        Optional<UserEntity> userEntity = userRepository.findById(userId);
-        List<PaymentEntity> paymentEntity = paymentRepository.findPaymentEntitiesByUser(userEntity.get());
-        if(userEntity !=null) {
-            for(PaymentEntity entity: paymentEntity){
-                PaymentGetDTO paymentGetDTO = PaymentConverter.toDTO(entity, new PaymentGetDTO());
-                Optional<CourseEntity> courseEntity = coursesRepository.findById(UUID.fromString(paymentGetDTO.getCourseId()));
-                courseEntities.add(courseEntity.get());
-            }
-        }else {
-            throw new IllegalStateException(
-                    "The userId " + userId + " does not exists."
-            );
-        }
+        UserEntity user = userRepository.findById(userId).orElseThrow(
+                () -> new HandleException("userId: "+userId+" does not exist"));
+        user.getPayment().stream().forEach(P -> courseEntities.add(P.getCourse()));
+
         return courseEntities;
+
     }
 }
