c6fcc82f4f315966027420e845bff6f4f2888af6
baobao <bluedev82@gmail.com>
2021-07-16 00:51:48 +0700

    finished

--- src/main/java/com/smartdev/ufoss/service/LessonsService.java
+++ src/main/java/com/smartdev/ufoss/service/LessonsService.java
@@ -1,11 +1,8 @@
 package com.smartdev.ufoss.service;
 
 import com.smartdev.ufoss.entity.LessonEntity;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +13,4 @@ public interface LessonsService {
     List<LessonEntity> uploadMultipleLesson(UUID courseId, MultipartFile[] lessons);
 
     void deleteLessonById(UUID courseId, UUID lessonsId);
-
-    ResponseEntity<Resource> getLessonVideo(UUID courseId, String fileName, HttpServletRequest request);
 }
